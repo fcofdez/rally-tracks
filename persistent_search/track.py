564cd81d41bccc443d82d7d38804aa27a670f240
Francisco Fernández Castaño <francisco.fernandez.castano@gmail.com>
2021-02-11 11:42:33 +0100

    First rally track for persistent search

---
+++ persistent_search/track.py
@@ -0,0 +1,25 @@
+from elasticsearch.client.utils import _make_path
+import asyncio
+
+
+def persistent_search(es, params):
+    es.transport.perform_request("POST", "/_persistent_search", body=params.get("body", "{}"))
+
+
+async def persistent_search_async(es, params):
+    index = params.get("index")
+    response = await es.transport.perform_request("POST", "/_persistent_search/", body=params.get("body", "{}"))
+    while True:
+        try:
+            response = await es.transport.perform_request("GET", _make_path("_persistent_search", response["id"]))
+            return
+        except Exception as e:
+            await asyncio.sleep(1)
+
+
+def register(registry):
+    async_runner = registry.meta_data.get("async_runner", False)
+    if async_runner:
+        registry.register_runner("persistent-search", persistent_search_async, async_runner=True)
+    else:
+        registry.register_runner("persistent-search", persistent_search)
